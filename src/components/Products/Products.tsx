a411e3fd630a31c16686cc359cb4c3524cc930a6
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-08-29 13:25:05 +0300

    added device page

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,28 +1,28 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
-import { useLocation } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
 import { selectProducts } from "../../store/slices/productsSlice";
 import { IDeviceStorage } from "../../types";
-import SQ from "../../utils/parseSearchQuery";
+// import SQ from "../../utils/parseSearchQuery";
 import Product from "../Product/Product";
 import st from "./Products.module.scss";
 
 const Products = () => {
     const { devices } = useSelector(selectProducts);
     const { t } = useTranslation();
-    const { search } = useLocation();
+    // const { search } = useLocation();
 
     const minPrice = (arr: IDeviceStorage[]) =>
         arr.reduce((prev, curr) => {
             return prev.price < curr.price ? prev : curr;
         });
 
-    const param = SQ.putParams(search, "set", {
-        value: "brands",
-        key: "xiaomi",
-    });
-    console.log(param);
+    // const param = SQ.putParams(search, "set", {
+    //     value: "brands",
+    //     key: "xiaomi",
+    // });
+    // console.log(param);
 
     return (
         <section className={st.root}>
