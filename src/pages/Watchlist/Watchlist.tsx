7c578a5c45437cdfaa273a657ccb65af79409017
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-08-30 15:20:58 +0300

    added watchlist and cart page

---
+++ src/pages/Watchlist/Watchlist.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const Watchlist: FC = () => {
+    return <div>WatchList</div>;
+};
+
+export default Watchlist;
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,10 +2,12 @@ import { Cart } from "./../pages/Cart/Cart";
 import LandingPage from "../pages/LandingPage/LandingPage";
 import ProductsPage from "../pages/ProductsPage/ProductsPage";
 import ProductPage from "../pages/ProductPage/ProductPage";
+import Watchlist from "../pages/Watchlist/Watchlist";
 
 export const routes = [
     { path: "/", element: LandingPage },
     { path: "/products", element: ProductsPage },
     { path: "/products/:slugParams", element: ProductPage },
     { path: "/cart", element: Cart },
+    { path: "/watchlist", element: Watchlist },
 ];
